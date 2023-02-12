42528afe673c85ba95eab6fb49113472f01bd0ac
jmk307 <jimin112688@gmail.com>
2023-02-12 16:41:51 +0900

    feat : 초기 프로젝트 세팅

---
+++ src/main/java/com/team6/sole/SsolBackendApplication.java
@@ -0,0 +1,23 @@
+package com.team6.sole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SsolBackendApplication extends SpringBootServletInitializer {
+
+	// ec2 404에러 해결 코드
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SsolBackendApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsolBackendApplication.class, args);
+	}
+
+}
