a8f6ca77818784bc4e6cfccace0663a108f8cbdd
강지민 <84773954+jmk307@users.noreply.github.com>
2024-01-18 12:23:53 +0000

    refactor : 중복로직들 함수화 및 핵심로직이 아닌 부분 다른 디렉토리로 이동

--- src/main/java/com/team6/sole/domain/member/MemberService.java
+++ src/main/java/com/team6/sole/domain/member/MemberService.java
@@ -1,37 +1,43 @@
 package com.team6.sole.domain.member;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Optional;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.team6.sole.domain.member.dto.*;
+import com.team6.sole.domain.member.dto.FcmTokenDto;
+import com.team6.sole.domain.member.dto.MemberRequestDto;
+import com.team6.sole.domain.member.dto.MemberResponseDto;
+import com.team6.sole.domain.member.dto.OauthRequest;
 import com.team6.sole.domain.member.entity.Accept;
 import com.team6.sole.domain.member.entity.Member;
 import com.team6.sole.domain.member.model.Social;
 import com.team6.sole.domain.scrap.ScrapFolderRespository;
 import com.team6.sole.domain.scrap.entity.ScrapFolder;
 import com.team6.sole.global.config.CommonApiResponse;
-import com.team6.sole.global.config.redis.RedisService;
 import com.team6.sole.global.config.s3.AwsS3ServiceImpl;
 import com.team6.sole.global.config.security.dto.TokenResponseDto;
 import com.team6.sole.global.config.security.jwt.TokenProvider;
 import com.team6.sole.global.config.security.oauth.AppleUtils;
+import com.team6.sole.global.config.security.oauth.KakaoUtils;
 import com.team6.sole.global.error.ErrorCode;
 import com.team6.sole.global.error.exception.BadRequestException;
 import com.team6.sole.global.error.exception.NotFoundException;
+
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.util.Optional;
+import static com.team6.sole.global.config.security.jwt.TokenProvider.*;
+import static com.team6.sole.global.config.security.oauth.KakaoUtils.*;
 
 @Service
 @RequiredArgsConstructor
@@ -41,19 +47,14 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final AcceptRepository acceptRepository;
     private final AwsS3ServiceImpl awsS3Service;
-    private final PasswordEncoder passwordEncoder;
     private final TokenProvider tokenProvider;
-    private final WebClient webClient;
-    private final AppleUtils appleUtils;
-
-    private static final String KAKAO = "kakao";
-    private static final String AUTHORIZATION = "Authorization";
-    private static final String BEARER = "Bearer ";
+    private final KakaoUtils kakaoUtils;
+    private final AppleUtils appleUtils;  
 
     // 회원체크 및 로그인(소셜)
-    @SneakyThrows // 명시적 예외처리(lombok)
+    @SneakyThrows
     @Transactional
-    public ResponseEntity<CommonApiResponse<MemberResponseDto>> checkMember(String provider, OauthRequest oauthRequest) {
+    public ResponseEntity<CommonApiResponse<MemberResponseDto>> checkMember(final String provider, final OauthRequest oauthRequest) {
         String socialCode = getSocialCode(provider, oauthRequest.getAccessToken());
         Social social = getSocial(provider);
         Optional<Member> checkMember = memberRepository.findBySocialIdAndSocial(socialCode, social);
@@ -71,34 +72,34 @@ public class MemberService {
     }
 
     // fcmToken null 체크
-    public String checkFcmToken(OauthRequest oauthRequest) {
+    public String checkFcmToken(final OauthRequest oauthRequest) {
         return oauthRequest.getFcmToken() == null
                 ? null
                 : oauthRequest.getFcmToken();
     }
 
     // Http Header 정보 생성
-    public HttpHeaders makeHttpHeaders(TokenResponseDto tokenResponseDTO) {
-        HttpHeaders httpHeaders = new HttpHeaders();
+    public HttpHeaders makeHttpHeaders(final TokenResponseDto tokenResponseDTO) {
+        final HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(AUTHORIZATION, BEARER + tokenResponseDTO.getAccessToken());
 
         return httpHeaders;
     }
 
     // 소셜 판별
-    public Social getSocial(String provider) {
+    public Social getSocial(final String provider) {
         return provider.equals(KAKAO)
                 ? Social.KAKAO
                 : Social.APPLE;
     }
 
     // 소셜 고유번호 가져오기
-    public String getSocialCode(String provider, String socialAccessToken)
+    public String getSocialCode(final String provider, final String socialAccessToken)
             throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeySpecException, JsonProcessingException {
         String socialCode = "";
 
         if (provider.equals(KAKAO)) {
-            socialCode = getKakaoUser(socialAccessToken).getAuthenticationCode();
+            socialCode = kakaoUtils.getKakaoMember(socialAccessToken).getAuthenticationCode();
         } else {
             socialCode = appleUtils.verifyPublicKey(socialAccessToken).get("sub").toString();
         }
@@ -109,24 +110,25 @@ public class MemberService {
     // 회원가입(소셜)
     @SneakyThrows // 명시적 예외처리(lombok)
     @Transactional
-    public ResponseEntity<CommonApiResponse<MemberResponseDto>> makeMember(String provider, MultipartFile multipartFile, MemberRequestDto memberRequestDto) {
+    public ResponseEntity<CommonApiResponse<MemberResponseDto>> makeMember(final String provider, final MultipartFile multipartFile, final MemberRequestDto memberRequestDto) {
         if (memberRepository.existsByNickname(memberRequestDto.getNickname())) {
             throw new BadRequestException(ErrorCode.MEMBER_ALREADY_EXIST);
         }
 
         String socialCode = getSocialCode(provider, memberRequestDto.getAccessToken());
         Social social = getSocial(provider);
-        String password = passwordEncoder.encode("social");
-        String profileImgUrl = multipartFile == null 
-                        ? null 
-                        : awsS3Service.uploadImage(multipartFile, "member");
+        String password = tokenProvider.makeStringEncoded("social");
+        String profileImgUrl = makeProfileImgUrl(multipartFile);
 
+        // 약관동의
         Accept accept = MemberRequestDto.acceptToEntity(memberRequestDto);
         acceptRepository.save(accept);
 
+        // 회원정보
         Member member = MemberRequestDto.memberToEntity(socialCode, password, social, profileImgUrl, accept, memberRequestDto);
         memberRepository.saveAndFlush(member);
 
+        // 스크랩폴더
         ScrapFolder scrapFolder = MemberRequestDto.scrapFolderToEntity(member, memberRequestDto);
         scrapFolderRespository.saveAndFlush(scrapFolder);
 
@@ -136,9 +138,16 @@ public class MemberService {
         return new ResponseEntity<>(CommonApiResponse.of(MemberResponseDto.of(member, tokenResponseDTO)), httpHeaders, HttpStatus.OK);
     }
 
+    // 첨부파일 -> AWS S3
+    public String makeProfileImgUrl(final MultipartFile multipartFile) {
+        return multipartFile == null 
+                ? null 
+                : awsS3Service.uploadImage(multipartFile, "member");
+    }
+
     // 토큰 재발급
     @Transactional
-    public ResponseEntity<CommonApiResponse<TokenResponseDto>> reissue(String accessToken, String refreshToken) {
+    public ResponseEntity<CommonApiResponse<TokenResponseDto>> reissue(final String accessToken, final String refreshToken) {
         String socialId;
 
         if (!tokenProvider.validateTokenExceptExpiration(accessToken)){
@@ -147,7 +156,7 @@ public class MemberService {
 
         try {
             socialId = tokenProvider.parseClaims(accessToken).getSubject();
-        } catch (Exception e) {
+        } catch (final Exception e) {
             throw new BadRequestException(ErrorCode.INVALID_REFRESH_TOKEN);
         }
 
@@ -164,7 +173,7 @@ public class MemberService {
     
     // fcmToken 교체
     @Transactional
-    public String modFcmToken(Member member, FcmTokenDto fcmTokenDto) {
+    public String modFcmToken(final Member member, final FcmTokenDto fcmTokenDto) {
         member.setFcmToken(fcmTokenDto.getFcmToken());
 
         return "fcmToken 교체 성공";
@@ -172,7 +181,7 @@ public class MemberService {
 
     // 로그아웃(fcmToken 삭제)
     @Transactional
-    public String logout(Member member) {
+    public String logout(final Member member) {
         /*//이미 로그아웃 된 상태
         if (StringUtils.isBlank(member.getFcmToken())) {
             throw new BadRequestException(ErrorCode.USER_ALREADY_LOGGED_OUT);
@@ -188,19 +197,18 @@ public class MemberService {
 
     // 닉네임 중복 검사
     @Transactional(readOnly = true)
-    public Boolean duplicateNickname(String nickname) {
+    public Boolean duplicateNickname(final String nickname) {
         return memberRepository.existsByNickname(nickname);
     }
 
     // 테스트 로그인
     @Transactional
-    public ResponseEntity<CommonApiResponse<MemberResponseDto>> checkMember(Long memberId) {
+    public ResponseEntity<CommonApiResponse<MemberResponseDto>> checkMember(final Long memberId) {
         Member checkMember = memberRepository.findById(memberId)
                 .orElseThrow(() -> new NotFoundException(ErrorCode.MEMBER_NOT_FOUND));
-
-        HttpHeaders httpHeaders = new HttpHeaders();
+       
         TokenResponseDto tokenResponseDTO = tokenProvider.generateToken(checkMember.getSocialId(), checkMember.getRole().name());
-        httpHeaders.add("Authorization", "Bearer " + tokenResponseDTO.getAccessToken());
+        HttpHeaders httpHeaders = makeHttpHeaders(tokenResponseDTO);
 
         log.info("로그인 성공");
 
@@ -209,25 +217,8 @@ public class MemberService {
 
     // 회원 삭제(중복 가입 방지용)
     @Transactional
-    public String delMember(Long memberId) {
+    public String delMember(final Long memberId) {
         memberRepository.deleteByMemberId(memberId);
         return "삭제 성공!";
-    }
-
-    // 카카오 유저 정보 가져오기
-    public KakaoUserDto getKakaoUser(String accessToken) {
-        String getUserURL = "https://kapi.kakao.com/v2/user/me";
-
-        try {
-            return webClient.post()
-                    .uri(getUserURL)
-                    .header(AUTHORIZATION, BEARER + accessToken)
-                    .retrieve()
-                    .bodyToMono(KakaoUserDto.class)
-                    .block();
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new BadRequestException(ErrorCode.KAKAO_BAD_REQUEST);
-        }
     }
 }
