159b55726fba654a1f90bfa0881885e1caa4699f
강지민 <84773954+jmk307@users.noreply.github.com>
2024-01-24 12:34:40 +0000

    refactor : 중복 기능 함수화

--- src/main/java/com/team6/sole/domain/history/HistoryService.java
+++ src/main/java/com/team6/sole/domain/history/HistoryService.java
@@ -9,6 +9,7 @@ import com.team6.sole.domain.home.entity.Place;
 import com.team6.sole.domain.home.model.PlaceCategory;
 import com.team6.sole.domain.home.model.TransCategory;
 import com.team6.sole.domain.home.repository.CourseCustomRepository;
+import com.team6.sole.domain.home.repository.CourseMemberRepository;
 import com.team6.sole.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,16 +22,14 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class HistoryService {
     private final CourseCustomRepository courseCustomRepository;
+    private final CourseMemberRepository courseMemberRepository;
 
     // 나의 기록 보기(상단)
     @Transactional(readOnly = true)
     public HistoryResponseDto showMyHistory(Member member) {
         List<PlaceCategory> placeCategories = new ArrayList<>();
         List<TransCategory> transCategories = new ArrayList<>();
-        for (Course course : member.getCourses()) {
-            placeCategories.addAll(course.getPlaceCategories());
-            transCategories.addAll(course.getTransCategories());
-        }
+        setCategories(member, placeCategories, transCategories);
 
         return HistoryResponseDto.of(
                 member,
@@ -39,48 +38,12 @@ public class HistoryService {
                 makeMostTransCategories(transCategories));
     }
 
-    // 나의 기록 보기(하단)(5개 + 5n)
-    @Transactional(readOnly = true)
-    public List<HomeResponseDto> showMyCourseHistories(Member member, Long courseId, HistorySearchRequestDto historySearchRequestDto) {
-        if (historySearchRequestDto == null) {
-            // 내가 기록한 코스 findAll
-            List<Course> courses = courseCustomRepository.findAllByWriter(courseId, member);
-            boolean finalPage = courses.size() - 1 != -1 && courseCustomRepository.findAllByWriter(
-                    courses.get(courses.size() - 1).getCourseId(),
-                    member).isEmpty();
-
-            return courses.stream()
-                    .map(course -> HomeResponseDto.of(
-                            course,
-                            true,
-                            finalPage))
-                    .collect(Collectors.toList());
+    // 멤버 선호카테고리 전체보기
+    public void setCategories(Member member, List<PlaceCategory> placeCategories, List<TransCategory> transCategories) {
+        for (Course course : member.getCourses()) {
+            placeCategories.addAll(course.getPlaceCategories());
+            transCategories.addAll(course.getTransCategories());
         }
-
-        // 내가 기록한 코스 중 검색 조건에 맞는 코스만 필터링
-        List<Course> filterCourses = courseCustomRepository.findAllByCatgegoryAndWriter(
-                courseId,
-                member,
-                historySearchRequestDto.getPlaceCategories(),
-                historySearchRequestDto.getTransCategories(),
-                historySearchRequestDto.getWithCategories(),
-                historySearchRequestDto.getRegions());
-
-        // 마지막 페이지 여부
-        boolean searchFinalPage = filterCourses.size() - 1 != -1 && courseCustomRepository.findAllByCatgegoryAndWriter(
-                filterCourses.get(filterCourses.size() - 1).getCourseId(),
-                member,
-                historySearchRequestDto.getPlaceCategories(),
-                historySearchRequestDto.getTransCategories(),
-                historySearchRequestDto.getWithCategories(),
-                historySearchRequestDto.getRegions()).isEmpty();
-
-        return filterCourses.stream()
-                .map(course -> HomeResponseDto.of(
-                        course,
-                        true,
-                        searchFinalPage))
-                .collect(Collectors.toList());
     }
 
     // 장소 최빈값
@@ -115,7 +78,7 @@ public class HistoryService {
                 .collect(Collectors.toList()).get(0);
     }
 
-    // 장소 카테고리 mapToSet
+    // 장소 카테고리 최빈값(mapToSet)
     public Set<PlaceCategory> makeMostPlaceCategories(List<PlaceCategory> placeCategories) {
         if (placeCategories.isEmpty()) {
             return Collections.emptySet();
@@ -141,7 +104,7 @@ public class HistoryService {
                 .collect(Collectors.toSet());
     }
 
-    // 대중교통 카테고리 mapToSet
+    // 대중교통 카테고리 최빈값(mapSet)
     public Set<TransCategory> makeMostTransCategories(List<TransCategory> transCategories) {
         if (transCategories.isEmpty()) {
             return Collections.emptySet();
@@ -165,5 +128,75 @@ public class HistoryService {
                 .map(Map.Entry::getKey)
                 .limit(1)
                 .collect(Collectors.toSet());
+    }
+
+    // 나의 기록 보기(하단)(5개 + 5n)
+    @Transactional(readOnly = true)
+    public List<HomeResponseDto> showMyCourseHistories(Member member, Long courseId, HistorySearchRequestDto historySearchRequestDto) {
+        if (historySearchRequestDto == null) {
+            // 내가 기록한 코스 findAll
+            List<Course> courses = courseCustomRepository.findAllByWriter(courseId, member);
+            return showRecentCourses(courses, member);
+        }
+
+        // 내가 기록한 코스 중 검색 조건에 맞는 코스만 필터링
+        List<Course> filterCourses = makeFilterCourses(member, courseId, historySearchRequestDto);
+
+        return showRecentFilterCourses(filterCourses, member, historySearchRequestDto);
+    }
+
+    // 최근 코스 모아보기
+    public List<HomeResponseDto> showRecentCourses(List<Course> courses, Member member) {
+        return courses.stream()
+                .map(recent -> HomeResponseDto.of(
+                        recent, 
+                        isCourseScrap(member, recent.getCourseId()), 
+                        isFinalPage(courses, member)))
+                .collect(Collectors.toList());
+    }
+
+    // 팔로잉한 사람들의 코스들 중 스크랩한 코스(T/F)
+    public Boolean isCourseScrap(Member member, Long courseId) {
+        return courseMemberRepository.existsByMemberAndCourse_CourseId(member, courseId);
+    }
+
+    // 마지막 페이지 판별(TODO -> PageDTO 클래스 따로파서 static method로)
+    public Boolean isFinalPage(List<Course> courses, Member member) {
+        return courses.size() - 1 != -1 
+                && courseCustomRepository.findAllByWriter(
+                        courses.get(courses.size() - 1).getCourseId(),
+                        member).isEmpty();
+    }
+
+    // 최근 코스 모아보기(검색 필터 추가)
+    public List<HomeResponseDto> showRecentFilterCourses(List<Course> filterCourses, Member member, HistorySearchRequestDto historySearchRequestDto) {
+        return filterCourses.stream()
+                .map(course -> HomeResponseDto.of(
+                        course,
+                        isCourseScrap(member, course.getCourseId()),
+                        isSearchFinalPage(filterCourses, member, historySearchRequestDto)))
+                .collect(Collectors.toList());
+    }
+
+    // 내가 기록한 코스 중 검색 조건에 맞는 코스만 필터링
+    public List<Course> makeFilterCourses(Member member, Long courseId, HistorySearchRequestDto historySearchRequestDto) {
+        return courseCustomRepository.findAllByCatgegoryAndWriter(
+            courseId,
+            member,
+            historySearchRequestDto.getPlaceCategories(),
+            historySearchRequestDto.getTransCategories(),
+            historySearchRequestDto.getWithCategories(),
+            historySearchRequestDto.getRegions());
+    }
+
+    // 마지막 검색페이지 판별(TODO -> PageDTO 클래스 따로파서 static method로)
+    public Boolean isSearchFinalPage(List<Course> filterCourses, Member member, HistorySearchRequestDto historySearchRequestDto) {
+        return filterCourses.size() - 1 != -1 && courseCustomRepository.findAllByCatgegoryAndWriter(
+            filterCourses.get(filterCourses.size() - 1).getCourseId(),
+            member,
+            historySearchRequestDto.getPlaceCategories(),
+            historySearchRequestDto.getTransCategories(),
+            historySearchRequestDto.getWithCategories(),
+            historySearchRequestDto.getRegions()).isEmpty();
     }
 }
